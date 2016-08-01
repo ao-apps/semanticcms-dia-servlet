866116ded7a0de1cb5f760a600214b6d7300a416
Dan Armstrong <dan@aoindustries.com>
2016-08-01 05:43:13 +0000

    Splitting dia into own projects.

--- src/com/aoindustries/web/dia/servlet/DiaFileOpener.java
+++ src/com/aoindustries/web/dia/servlet/DiaFileOpener.java
@@ -29,6 +29,9 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+/**
+ * @see  OpenFile.FileOpener
+ */
 @WebListener(
 	"Registers a file opener for all *." + DiaExport.EXTENSION + " files."
 )
