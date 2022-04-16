65be1a42b830c0cba6627796b4d87b0928b8fcc1
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:45 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/dia/servlet/impl/DiaImpl.java
+++ src/main/java/com/semanticcms/dia/servlet/impl/DiaImpl.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-dia-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.dia.servlet.impl;
 
 import com.aoapps.concurrent.KeyedConcurrencyReducer;
