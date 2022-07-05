9322ed4406a071c1de459c9aa738556dc51fa4c3
AO Industries, Inc <support@aoindustries.com>
2022-07-05 03:29:51 -0500

    Added copyright from master branch

--- src/main/java/com/semanticcms/dia/servlet/impl/DiaExport.java
+++ src/main/java/com/semanticcms/dia/servlet/impl/DiaExport.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-dia-servlet - Java API for embedding Dia-based diagrams in web pages in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
