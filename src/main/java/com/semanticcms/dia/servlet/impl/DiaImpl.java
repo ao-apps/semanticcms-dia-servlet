66dbcf2fc879d182a374e05837558d6de54e6f7a
AO Industries, Inc <support@aoindustries.com>
2024-08-31 19:21:26 -0500

    checkstyle-10.18.1 compatibility

--- src/main/java/com/semanticcms/dia/servlet/impl/DiaImpl.java
+++ src/main/java/com/semanticcms/dia/servlet/impl/DiaImpl.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-dia-servlet - Java API for embedding Dia-based diagrams in web pages in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -475,7 +475,8 @@ public final class DiaImpl {
                   script.write(") ");
                 }
                 script.append("{\n"
-                    + "        document.getElementById(").text(refId).append(").src = document.getElementById(\"" + ALT_LINK_ID_PREFIX).append(Long.toString(altLinkNum)).append("\").getAttribute(\"href\");\n"
+                    + "        document.getElementById(").text(refId).append(").src = document.getElementById(\""
+                        + ALT_LINK_ID_PREFIX).append(Long.toString(altLinkNum)).append("\").getAttribute(\"href\");\n"
                     + "      }\n");
               }
               script.write("    }\n"
